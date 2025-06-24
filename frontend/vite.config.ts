bb917571ea9b85cf4372cc26beff2b1ed3993bd3
Jessica <jessica.santosb@outlook.com>
2025-06-24 14:28:42 -0300

    fix: add allowed hosts for Vite server configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
       usePolling: true,
     },
     host: true,
+    allowedHosts: [
+      'ec2-3-142-212-163.us-east-2.compute.amazonaws.com',
+      'localhost',
+    ],
     strictPort: true,
     port: 5173,
     proxy: {
