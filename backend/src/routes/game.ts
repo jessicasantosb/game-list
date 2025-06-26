f849aaecdde8bc6fc76b4823aa3c1b5b63fa3561
Jessica <jessica.santosb@outlook.com>
2025-06-26 18:10:42 -0300

    fix: update route paths for games and platforms to use plural form

--- backend/src/routes/game.ts
+++ backend/src/routes/game.ts
@@ -6,20 +6,20 @@ import { ensureAuthentication } from '@/middleware/ensureAuthentication';
 const routes = Router();
 
 routes.get(
-  '/game',
+  '/games',
   ensureAuthentication,
   gamesControllers.getAllValidator,
   gamesControllers.getAll,
 );
 
-routes.post('/game', ensureAuthentication, gamesControllers.create);
+routes.post('/games', ensureAuthentication, gamesControllers.create);
 
-routes.get('/game/:id', ensureAuthentication, gamesControllers.getById);
+routes.get('/games/:id', ensureAuthentication, gamesControllers.getById);
 
-routes.put('/game/:id', ensureAuthentication, gamesControllers.updateById);
+routes.put('/games/:id', ensureAuthentication, gamesControllers.updateById);
 
-routes.delete('/game/:id', ensureAuthentication, gamesControllers.deleteById);
+routes.delete('/games/:id', ensureAuthentication, gamesControllers.deleteById);
 
-routes.patch('/game/:id', ensureAuthentication, gamesControllers.favoriteById);
+routes.patch('/games/:id', ensureAuthentication, gamesControllers.favoriteById);
 
 export { routes };
