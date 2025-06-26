0ba3f6f7e4634c1dc661b2e7394943c4416f6e9c
Jessica <jessica.santosb@outlook.com>
2025-06-26 16:22:56 -0300

    style: improve sidebar responsiveness

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -5,17 +5,12 @@ import { useSidebar } from '../../hooks/useSidebar';
 import { useUser } from '../../hooks/useUser';
 import {
   category,
-  categoryHover,
   compactLogo,
   fullLogo,
   game,
-  gameHover,
   home,
-  homeHover,
-  logoutHover,
   logout as logoutIcon,
   platform,
-  platformHover,
 } from '../../utils/icons';
 import { Button } from '../ui/button/Button';
 import styles from './Sidebar.module.css';
@@ -28,118 +23,67 @@ const Sidebar: React.FC = () => {
     isActive ? { backgroundColor: '#42D9C8', color: '#fff' } : undefined;
 
   return (
-    <>
-      <button className={styles.menuButton} onClick={toggleSidebar}>
+    <aside
+      className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
+      <Button className={styles.menuButton} onClick={toggleSidebar}>
         ☰
-      </button>
+      </Button>
+      <div className={styles.header}>
+        {isOpen ? (
+          <div className={styles.logo}>
+            <img src={fullLogo} alt='Logo' />
+          </div>
+        ) : (
+          <img
+            src={compactLogo}
+            alt='Logo Compact'
+            className={styles.logoCompact}
+          />
+        )}
+      </div>
 
-      <aside
-        className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
-        <div className={styles.header}>
-          {isOpen ? (
-            <div className={styles.logo}>
-              <img src={fullLogo} alt='Logo' />
-            </div>
-          ) : (
-            <img
-              src={compactLogo}
-              alt='Logo Compact'
-              className={styles.logoCompact}
-            />
-          )}
-        </div>
+      <nav className={styles.nav}>
+        <NavLink
+          to='/'
+          style={navLinkStyle}
+          className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
+          <img src={home} alt='Home' className={styles.icon} />
+          {isOpen && <span>Home</span>}
+        </NavLink>
 
-        <nav className={styles.nav}>
-          <NavLink
-            to='/'
-            style={navLinkStyle}
-            className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
-            <div className={styles.iconWrapper}>
-              <img src={home} alt='Home' className={styles.iconDefault} />
-              <img
-                src={homeHover}
-                alt='Home Hover'
-                className={styles.iconHover}
-              />
-            </div>
-            {isOpen && <span>Home</span>}
-          </NavLink>
+        <NavLink
+          style={navLinkStyle}
+          to='/games'
+          className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
+          <img src={game} alt='Games' className={styles.icon} />
+          {isOpen && <span>Games</span>}
+        </NavLink>
 
-          <NavLink
-            style={navLinkStyle}
-            to='/games'
-            className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
-            <div className={styles.iconWrapper}>
-              <img src={game} alt='Games' className={styles.iconDefault} />
-              <img
-                src={gameHover}
-                alt='Games Hover'
-                className={styles.iconHover}
-              />
-            </div>
-            {isOpen && <span>Games</span>}
-          </NavLink>
+        <NavLink
+          style={navLinkStyle}
+          to='/categories'
+          className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
+          <img src={category} alt='Categories' className={styles.icon} />
+          {isOpen && <span>Categories</span>}
+        </NavLink>
 
-          <NavLink
-            style={navLinkStyle}
-            to='/categories'
-            className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
-            <div className={styles.iconWrapper}>
-              <img
-                src={category}
-                alt='Categories'
-                className={styles.iconDefault}
-              />
-              <img
-                src={categoryHover}
-                alt='Categories Hover'
-                className={styles.iconHover}
-              />
-            </div>
-            {isOpen && <span>Categories</span>}
-          </NavLink>
+        <NavLink
+          style={navLinkStyle}
+          to='/platforms'
+          className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
+          <img src={platform} alt='Platforms' className={styles.icon} />
+          {isOpen && <span>Platforms</span>}
+        </NavLink>
+      </nav>
 
-          <NavLink
-            style={navLinkStyle}
-            to='/platforms'
-            className={`${styles.navItem} ${!isOpen ? styles.navItemClosed : ''}`}>
-            <div className={styles.iconWrapper}>
-              <img
-                src={platform}
-                alt='Platforms'
-                className={styles.iconDefault}
-              />
-              <img
-                src={platformHover}
-                alt='Platforms Hover'
-                className={styles.iconHover}
-              />
-            </div>
-            {isOpen && <span>Platforms</span>}
-          </NavLink>
-        </nav>
+      <Button
+        onClick={logout}
+        className={`${styles.navItem} ${styles.logout} ${!isOpen ? styles.logoutClosed : ''}`}>
+        {isOpen && <span>Logout</span>}
 
-        <div className={styles.logout}>
-          <Button
-            onClick={logout}
-            className={`${styles.navItem} ${!isOpen ? styles.logoutClosed : ''}`}>
-            {isOpen && <span>Logout</span>}
-            <div className={styles.iconWrapper}>
-              <img
-                src={logoutIcon}
-                alt='Logout'
-                className={styles.logoutIconDefault}
-              />
-              <img
-                src={logoutHover}
-                alt='Logout Hover'
-                className={styles.logoutIconHover}
-              />
-            </div>
-          </Button>
-        </div>
-      </aside>
-    </>
+        <img src={logoutIcon} alt='Logout' className={styles.logoutIcon} />
+      </Button>
+    </aside>
   );
 };
 
